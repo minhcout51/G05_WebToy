6168c09a765c47f5bd620de2a733fd5f220d3899
Minh Ngọc <ngcminh007@gmail.com>
2023-04-19 09:14:51 +0700

    Add project files.

---
+++ WebToy/wwwroot/js/dialog.js
@@ -0,0 +1,6 @@
+﻿window.onload = function () {
+    document.getElementById('targetButton').onclick = function () {
+        var dialog = document.getElementById("dialog").ej2_instances[0]
+        dialog.show();
+    }
+}
