30600a86668e805f9e83af820eaaffeec845ca90
minhcout51 <ngcminh007@gmail.com>
2023-04-28 16:32:54 +0700

    Making product list

--- WebToy/Models/Product.cs
+++ WebToy/Models/Product.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace WebToy.Models
 {
-    public class SanPham
+    public class Product
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
